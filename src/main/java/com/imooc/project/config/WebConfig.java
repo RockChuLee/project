bf80c926d78c7f38c68d0875eee26adcae96e3e9
dong <donghao@fang.com>
2021-05-24 10:15:50 +0800

    登录功能：前端代码

---
+++ src/main/java/com/imooc/project/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.imooc.project.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @Description:
+ * @Author: dh
+ * @Date: 2021/5/24 10:12
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("login/login");
+    }
+}
